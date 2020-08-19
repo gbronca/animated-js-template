d824fbc33d0476605f0ce6005ec197c58ccd83be
gbronca <42703789+gbronca@users.noreply.github.com>
2020-08-19 19:51:32 +0100

    added js code to .js files

---
+++ script.js
@@ -0,0 +1,5 @@
+AOS.init({
+	// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+	delay: 200, // values from 0 to 3000, with step 50ms
+	duration: 1500, // values from 0 to 3000, with step 50ms
+});
